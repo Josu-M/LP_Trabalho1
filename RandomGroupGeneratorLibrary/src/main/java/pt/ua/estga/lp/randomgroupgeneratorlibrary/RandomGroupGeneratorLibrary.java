f2fd18be4b2b379b33fbaaa1c051d92eece14655
SebastianMazoSimoes <sebastianmsimoes@ua.pt>
2026-02-26 17:11:16 +0000

    2º commit

--- RandomGroupGeneratorLibrary/src/main/java/pt/ua/estga/lp/randomgroupgeneratorlibrary/RandomGroupGeneratorLibrary.java
+++ RandomGroupGeneratorLibrary/src/main/java/pt/ua/estga/lp/randomgroupgeneratorlibrary/RandomGroupGeneratorLibrary.java
@@ -3,14 +3,46 @@
  */
 
 package pt.ua.estga.lp.randomgroupgeneratorlibrary;
-
+import java.util;
 /**
  *
  * @author josue
  */
 public class RandomGroupGeneratorLibrary {
 
-    public static void main(String[] args) {
+//Classe Estudante:
+    public class Estudante{
         
-    }
+//Atributos:
+        private String nome;
+        private int nmec;
+        
+//Construtor para criar o objeto estudante e os seus dados:
+        public Estudante (String nome, int nmec)
+        {
+        this.nome = nome;
+        this.nmec = nmec;
+        }
+        
+ //Getters para o nome e nmec, Permite o acesso destes dados para outras partes do código
+        public String getnome()
+        {
+            return nome;
+        }
+        
+        public int getnmec()
+        {
+            return nmec;
+        }
+        
+        
+        
+        
+        
+}
+    
+    
+    
+    
+    
 }
