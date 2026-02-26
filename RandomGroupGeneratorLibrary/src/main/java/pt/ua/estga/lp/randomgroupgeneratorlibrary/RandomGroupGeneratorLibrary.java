2d9988b569547ab8cb19cabe21727d3369b0617f
Josu-M <josuemarques@ua.pt>
2026-02-26 15:57:16 +0000

    Biblioteca RandomGroupGeneratorLibrary

---
+++ RandomGroupGeneratorLibrary/src/main/java/pt/ua/estga/lp/randomgroupgeneratorlibrary/RandomGroupGeneratorLibrary.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pt.ua.estga.lp.randomgroupgeneratorlibrary;
+
+/**
+ *
+ * @author josue
+ */
+public class RandomGroupGeneratorLibrary {
+
+    public static void main(String[] args) {
+        
+    }
+}
