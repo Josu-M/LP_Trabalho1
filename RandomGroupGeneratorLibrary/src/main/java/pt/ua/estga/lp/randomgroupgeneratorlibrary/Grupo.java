06c56f431c675de3742f2d2b3e4929540f06aab6
SebastianMazoSimoes <sebastianmsimoes@ua.pt>
2026-02-27 10:41:48 +0000

    4º commit

--- RandomGroupGeneratorLibrary/src/main/java/pt/ua/estga/lp/randomgroupgeneratorlibrary/Grupo.java
+++ RandomGroupGeneratorLibrary/src/main/java/pt/ua/estga/lp/randomgroupgeneratorlibrary/Grupo.java
@@ -1,9 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package pt.ua.estga.lp.randomgroupgeneratorlibrary;
 
-public class RandomGroupGeneratorLibrary {
+/**
+ *
+ * @author SebastianMazoSimoes
+ */
+public class Grupo {
     
 }
