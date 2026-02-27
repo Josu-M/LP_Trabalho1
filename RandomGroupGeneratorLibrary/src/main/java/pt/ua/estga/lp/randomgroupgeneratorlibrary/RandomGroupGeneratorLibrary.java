b8580a3cfdc4dfc661a434eaf2818a2f42e1810c
Josu-M <josuemarques@ua.pt>
2026-02-27 11:53:08 +0000

    Sem classe estudante no ficheiro random

--- RandomGroupGeneratorLibrary/src/main/java/pt/ua/estga/lp/randomgroupgeneratorlibrary/RandomGroupGeneratorLibrary.java
+++ RandomGroupGeneratorLibrary/src/main/java/pt/ua/estga/lp/randomgroupgeneratorlibrary/RandomGroupGeneratorLibrary.java
@@ -3,15 +3,8 @@
  */
 
 package pt.ua.estga.lp.randomgroupgeneratorlibrary;
-import java.util;
+import java.util.*;
 
 public class RandomGroupGeneratorLibrary {
-<<<<<<< HEAD
-    public class Estudante {
-        
-    }
-}
-=======
     
 }
->>>>>>> 06c56f431c675de3742f2d2b3e4929540f06aab6
